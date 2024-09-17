90dd9f0c31b6562842d59eb0b2c154345aec324b
Ssekyene Robert <robertssekyene05@gmail.com>
2024-09-17 07:21:46 +0000

    added link on the logo

--- static/scripts/header.js
+++ static/scripts/header.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
   // Open the mobile nav menu
-  $('.menu_icon').click(function () {
+  $('.menu_icon').click(function() {
       $('.nav_links').addClass('active');
       $('.close_icon').addClass('active');
       $('body').addClass('nav_active');
     });
     
     // Close the mobile nav menu
-    $('.close_icon').click(function () {
+    $('.close_icon').click(function() {
       $('.nav_links').removeClass('active');
       $('.close_icon').removeClass('active');
       $('body').removeClass('nav_active');
+    });
+
+    $('div#header_logo').click(function() {
+      window.location.href = '/home'
     });
 });
